3ab3c1138042397e2eceaa4ef0126420b16ba777
jean krupa <jkrupa@hotmail.fr>
2024-10-01 21:59:18 +0200

    added seo stuff

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -3,6 +3,18 @@ module PagesHelper
     content_for(:title) { page_title }
   end
 
+  def description(page_description)
+    content_for(:description) { page_description }
+  end
+
+  def canonical_link_tag
+    tag.link rel: "canonical", href: request.original_url.split("?").first
+  end
+
+  def keywords(page_keywords)
+    content_for(:keywords) { page_keywords.join(", ") }
+  end
+
   def viewport_meta_tag(content: "width=device-width, initial-scale=1", turbo_native: "maximum-scale=1.0, user-scalable=0")
     full_content = [content, (turbo_native if turbo_native_app?)].compact.join(", ")
     tag.meta name: "viewport", content: full_content
